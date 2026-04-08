6ff699c21e5d8339420af41a7fdb891555b55930
AlainZhangStudent <superuser@DESKTOP-E9R0TKE.localdomain>
2026-04-08 00:12:40 -0700

    remove generated doxygen output

--- html/search/classes_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['spawnpacket_0',['SpawnPacket',['../structshared_1_1SpawnPacket.html',1,'shared']]],
-  ['stateentry_1',['StateEntry',['../structshared_1_1StateEntry.html',1,'shared']]],
-  ['stateheader_2',['StateHeader',['../structshared_1_1StateHeader.html',1,'shared']]]
-];
