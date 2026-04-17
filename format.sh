fdf741a9238ceae72e6c421d5a978b07a5118ce6
Ziyue Liu <ziyueliu@Ziyues-MacBook-Pro.local>
2026-04-17 13:50:16 -0700

    implemented the test for the pipe

--- format.sh
+++ format.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 find src -name '*.cpp' -o -name '*.h' | xargs clang-format -i
+find src -name '*.cpp' | xargs clang-tidy --fix -p build --header-filter='.*' --exclude-header-filter='(lib|build)/.*'
