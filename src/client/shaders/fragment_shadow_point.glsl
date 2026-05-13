312104e0e6a497b01866c3be292e21e3810b15a3
Ziyue Liu <ziyueliu@Ziyues-MacBook-Pro.attlocal.net>
2026-05-13 08:24:05 -0700

    fix on mac

--- src/client/shaders/fragment_shadow_point.glsl
+++ src/client/shaders/fragment_shadow_point.glsl
@@ -1,12 +1,10 @@
 #version 410 core
 in vec4 fragWorldPos;
-flat in int lightIdx;
 
-uniform vec3 lightPositions[K_MAX_POINT_LIGHTS];
+uniform vec3 lightPosition;
 uniform float pointFarPlane;
 
 void main() {
-  // Linear distance / pointFarPlane → [0,1] depth attachment.
-  float d = length(fragWorldPos.xyz - lightPositions[lightIdx]);
+  float d = length(fragWorldPos.xyz - lightPosition);
   gl_FragDepth = clamp(d / pointFarPlane, 0.0, 1.0);
 }
