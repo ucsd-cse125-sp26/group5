bf7127219508f313de136aca8d8dbae2835bc605
Ziyue Liu <ziyueliu@Ziyues-MacBook-Pro.local>
2026-06-02 20:40:26 -0700

    finished the summer game

--- src/server/server_level_loader.cpp
+++ src/server/server_level_loader.cpp
@@ -102,7 +102,7 @@ void loadLevel(ServerGame& game) {
   auto [springSectionID, springSection] = new_entity(game);
   game.registry.emplace<shared::SectionController>(
       springSection, shared::SectionSeasonMap::SPRING, puzzleTengramID,
-      false,  // spring is 4th to unlock
+      true,  // spring is 4th to unlock
       false   // not completed yet
   );
 
