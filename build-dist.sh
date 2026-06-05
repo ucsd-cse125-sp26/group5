eea4533943d97f20e203cca11867da55a6a264a1
LC0229 <shc067@ucsd.edu>
2026-06-05 10:54:26 -0700

    {cmake_extra[@]}

--- build-dist.sh
+++ build-dist.sh
@@ -68,10 +68,17 @@ esac
 
 if [[ $do_build -eq 1 ]]; then
 	echo "==> Configuring $variant Windows build in $build_dir"
-	cmake -B "$build_dir" -G Ninja \
-		-DCMAKE_TOOLCHAIN_FILE=toolchains/windows.cmake \
-		-DCMAKE_BUILD_TYPE=Release \
-		"${cmake_extra[@]}"
+	# macOS ships Bash 3.2; "${array[@]}" on an empty array trips `set -u`.
+	if [[ ${#cmake_extra[@]} -gt 0 ]]; then
+		cmake -B "$build_dir" -G Ninja \
+			-DCMAKE_TOOLCHAIN_FILE=toolchains/windows.cmake \
+			-DCMAKE_BUILD_TYPE=Release \
+			"${cmake_extra[@]}"
+	else
+		cmake -B "$build_dir" -G Ninja \
+			-DCMAKE_TOOLCHAIN_FILE=toolchains/windows.cmake \
+			-DCMAKE_BUILD_TYPE=Release
+	fi
 	echo "==> Building"
 	(cd "$build_dir" && ninja)
 fi
